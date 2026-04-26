176f9eb1a9882629cfc5b618d89fe975a51bbc73
Chirayu Shah <chirayushah61@gmail.com>
2026-04-25 21:41:16 -0600

    added laptop config

--- flake.nix
+++ flake.nix
@@ -9,23 +9,41 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-
   outputs = { self, nixpkgs, home-manager, ... }@inputs: {
-    nixosConfigurations.NixLatitude = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
+    nixosConfigurations = {
+      # Desktop Configuration
+      NixHome = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.rayu = import ./home.nix;
+          }
+          {
+            fileSystems."/mnt/storage" = {
+              device = "/dev/disk/by-uuid/1456bb2e-df41-479f-acae-868420c1bc3a";
+              fsType = "ext4";
+            };
+          }
+        ];
+      };
 
-      modules = [
-        ./configuration.nix
-
-        home-manager.nixosModules.home-manager
-
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-
-          home-manager.users.rayu = import ./home.nix;
-        }
-      ];
+      # Laptop Configuration
+      NixPrecision = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.rayu = import ./home.nix;
+          }
+        ];
+      };
     };
   };
 }
