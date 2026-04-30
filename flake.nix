4fa93f8965708d48445d52bb4214566450afb833
Chirayu Shah <chirayushah61@gmail.com>
2026-04-30 14:08:14 -0600

    removed windows and created a new mount

--- flake.nix
+++ flake.nix
@@ -36,6 +36,10 @@
               device = "/dev/disk/by-uuid/1456bb2e-df41-479f-acae-868420c1bc3a";
               fsType = "ext4";
             };
+            fileSystems."/mnt/storage2" = {
+              device = "/dev/disk/by-uuid/7b49a9b6-3126-4a4e-9ef1-23f3223d377d";
+              fsType = "ext4";
+            };
           }
         ];
       };
