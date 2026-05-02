42b256120c7c4a831c201ab682089c05729fc23c
Chirayu Shah <chirayushah61@gmail.com>
2026-05-01 18:01:13 -0600

    prime offloading

--- laptop.nix
+++ laptop.nix
@@ -4,7 +4,32 @@
   # users.users.rayu.packages = lib.mkForce [];
   systemd.services.NetworkManager-wait-online.enable = false;
 
-  boot.blacklistedKernelModules = [ "nouveau" "nvidia" "nvidia_drm" "nvidia_modeset" ];
-  services.xserver.videoDrivers = [ "modesetting" ];
-  hardware.nvidia.modesetting.enable = lib.mkForce false;
+  # Load the Nvidia driver (required for power management)
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.nvidia = {
+    # Required for Niri
+    modesetting.enable = true;
+    # Open source drivers
+    open = true; 
+
+    # Enable the Nvidia settings menu
+    nvidiaSettings = true;
+  
+    powerManagement = {
+      enable = true;
+      finegrained = true;
+    };
+
+    prime = {
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
+      
+      # hardware Bus IDs
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0"; 
+    };
+  };
 }
