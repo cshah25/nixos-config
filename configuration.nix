c3d10261bcd3c97aa9b4400c092ec96d00848cef
Chirayu Shah <chirayushah61@gmail.com>
2026-05-01 23:00:25 -0600

    removed gaps

--- configuration.nix
+++ configuration.nix
@@ -29,7 +29,7 @@
   # Use latest kernel.
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  networking.hostName = "NixPrecision"; # Define your hostname.
+  networking.hostName = "NixHome"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   nix.settings.experimental-features = ["nix-command" "flakes"];
