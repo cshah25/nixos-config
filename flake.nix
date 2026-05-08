059f034b5775fde83809ed8330b9644348c225da
Chirayu Shah <chirayushah61@gmail.com>
2026-05-07 20:46:06 -0600

    refactored flake.nix

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.rayu = import ./home.nix;
+            home-manager.users.rayu = import ./home;
             home-manager.backupFileExtension = "backup";
           }
           {
@@ -49,7 +49,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.rayu = import ./home.nix;
+            home-manager.users.rayu = import ./home/home.nix;
             home-manager.backupFileExtension = "backup";
           }
           {
