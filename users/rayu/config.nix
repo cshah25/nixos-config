91b9b80974dce30aa4ba5026e1a9c01aaa0d807c
Chirayu Shah <chirayushah61@gmail.com>
2026-05-09 20:51:25 -0600

    added noctalia

--- users/rayu/config.nix
+++ users/rayu/config.nix
@@ -9,6 +9,7 @@
 	 "niri".source = ./dotfiles/niri;
 	 "qt5ct".source = ./dotfiles/qt5ct;
    "nvim".source = ./dotfiles/nvim;
+   "noctalia".source = ./dotfiles/noctalia;
   };
   home.file.".p10k.zsh".source = ./dotfiles/p10k.zsh;
 
