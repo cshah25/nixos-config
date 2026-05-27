96d04d90b6238c8bd2d76be6191ec943106b51ea
Chirayu Shah <chirayushah61@gmail.com>
2026-05-26 19:44:39 -0600

    removed 2tb ssd from desktop and updated displays

--- hosts/NixHome/default.nix
+++ hosts/NixHome/default.nix
@@ -36,12 +36,12 @@
       allowedUDPPorts = [ 9 ];
     };
   };
-
-  fileSystems."/mnt/storage" = {
-    device = "/dev/disk/by-uuid/1456bb2e-df41-479f-acae-868420c1bc3a";
-    fsType = "ext4";
-    options = [ "defaults" "nofail" "X-systemd.device-timeout=5s" ];
-  };
+  # 2TB SSD
+  #fileSystems."/mnt/storage" = {
+  #  device = "/dev/disk/by-uuid/1456bb2e-df41-479f-acae-868420c1bc3a";
+  #  fsType = "ext4";
+  #  options = [ "defaults" "nofail" "X-systemd.device-timeout=5s" ];
+  #};
 
   fileSystems."/mnt/storage2" = {
     device = "/dev/disk/by-uuid/7b49a9b6-3126-4a4e-9ef1-23f3223d377d";
