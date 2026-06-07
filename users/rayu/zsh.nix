ad648ec1781fb0bf3c0a8db129dffee0ac4eced7
Chirayu Shah <chirayushah61@gmail.com>
2026-06-07 09:50:41 -0600

    zsh alias

--- users/rayu/zsh.nix
+++ users/rayu/zsh.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, hostname, ... }:
 
 {
   programs.zsh = {
@@ -28,6 +28,22 @@
       # Sourcing the p10k configuration file
       source ${./dotfiles/p10k.zsh}
 
+      # Nix commands
+      alias nrs='sudo nixos-rebuild switch --flake /home/rayu/nixos-config/#${hostname}'
+      alias nrb='sudo nixos-rebuild boot --flake /home/rayu/nixos-config/#${hostname}'     
+      alias nfu='nix flake update'
+      
+      # Git commands
+      alias gs='git status'
+      alias ga='git add'
+      alias gc='git commit -m'
+      alias gp='git push'
+      alias gpl='git pull'
+      alias gf='git fetch'
+      alias gcl='git clone'
+      alias gl='git log'
+
+      # Distrobox 
       pybox() {
         if [[ -z "$1" ]]; then
             echo "Error: No target directory provided."
