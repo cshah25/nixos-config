244169ff3ccfb11ce63db6c20675d768e4cfb105
Chirayu Shah <chirayushah61@gmail.com>
2026-07-09 22:36:57 -0600

    display update

--- users/rayu/niri.nix
+++ users/rayu/niri.nix
@@ -18,7 +18,7 @@
     '';
 
     "niri/cfg/display.kdl".text = if hostname == "NixHome" then ''
-      output "DP-4" {
+      output "DP-3" {
           mode "3840x2160@144.001"
           scale 1.5
           position x=2560 y=0
