01168a26322e9b705e44a4924ccfb9952d274b17
Chirayu Shah <chirayushah61@gmail.com>
2026-07-17 08:39:13 -0600

    p10k mode update

--- users/rayu/zsh.nix
+++ users/rayu/zsh.nix
@@ -25,9 +25,6 @@
     ];
 
     initContent = ''
-      # Sourcing the p10k configuration file
-      source ${./dotfiles/p10k.zsh}
-
       # Nix commands
       alias nrs='sudo nixos-rebuild switch --flake /home/rayu/nixos-config/#${hostname}'
       alias nrb='sudo nixos-rebuild boot --flake /home/rayu/nixos-config/#${hostname}'     
