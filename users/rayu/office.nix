5897dea13e6b01610068a148ddbcdf7f527fecce
Chirayu Shah <chirayushah61@gmail.com>
2026-07-23 22:23:42 -0600

    fixed mailspring keyring error

--- users/rayu/office.nix
+++ users/rayu/office.nix
@@ -1,9 +1,19 @@
-{ config, pkgs, pkgs-stable, osConfig, ... }:
+{ config, pkgs, pkgs-stable, osConfig, lib, ... }:
 
-{
-  home.packages = if osConfig.sys.office.enable then [ 
+lib.mkIf osConfig.sys.office.enable {
+  home.packages = [ 
     pkgs-stable.libreoffice 
     pkgs-stable.zoom-us
     pkgs-stable.mailspring
-  ] else [];
+  ];
+
+  xdg.desktopEntries.mailspring = {
+    name = "Mailspring";
+    comment = "The best email app for people and teams.";
+    exec = "mailspring --password-store=\"gnome-libsecret\" %U";
+    icon = "mailspring";
+    terminal = false;
+    categories = [ "Network" "Email" ];
+    mimeType = [ "x-scheme-handler/mailto" "x-scheme-handler/mailspring" ];
+  };
 }
