bdda012c40c9a8ef4559115b7da719bd1bcf5793
Chirayu Shah <chirayushah61@gmail.com>
2026-07-23 22:46:28 -0600

    fixed mailspring

--- users/rayu/office.nix
+++ users/rayu/office.nix
@@ -1,13 +1,24 @@
 { config, pkgs, pkgs-stable, osConfig, lib, ... }:
 
+let
+  mailspring-wrapped = pkgs.symlinkJoin {
+    name = "mailspring-wrapped";
+    paths = [ pkgs-stable.mailspring ];
+    buildInputs = [ pkgs.makeWrapper ];
+    postBuild = ''
+      wrapProgram $out/bin/mailspring \
+        --add-flags '--password-store="gnome-libsecret"'
+    '';
+  };
+in
 lib.mkIf osConfig.sys.office.enable {
   home.packages = [ 
     pkgs-stable.libreoffice 
     pkgs-stable.zoom-us
-    pkgs-stable.mailspring
+    mailspring-wrapped
   ];
 
-  xdg.desktopEntries.mailspring = {
+  xdg.desktopEntries.Mailspring = {
     name = "Mailspring";
     comment = "The best email app for people and teams.";
     exec = "mailspring --password-store=\"gnome-libsecret\" %U";
