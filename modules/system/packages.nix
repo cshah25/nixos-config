9b83160f85e4ce8482f7e0526eeb86dff25856c2
Chirayu Shah <chirayushah61@gmail.com>
2026-08-22 09:37:44 -0600

    extensions

--- modules/system/packages.nix
+++ modules/system/packages.nix
@@ -34,6 +34,17 @@
       kitty
     ] ++ lib.optionals config.sys.desktop.plasma.enable [
       kdePackages.kwalletmanager
+    ] ++ lib.optionals config.sys.desktop.gnome.enable [
+      gnomeExtensions.blur-my-shell
+      gnomeExtensions.caffeine
+      gnomeExtensions.clipboard-indicator
+      gnomeExtensions.dash-to-dock
+      gnomeExtensions.appindicator
+      gnomeExtensions.multi-monitor-bar
+      gnomeExtensions.removable-drive-menu
+    ] ++ lib.optionals (config.sys.desktop.gnome.enable && config.networking.hostName == "NixHome") [
+      ddcutil
+      gnomeExtensions.brightness-control-using-ddcutil
     ];
   };
 }
