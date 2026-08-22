b12e7c6fb4266e707a0e1394868b787344ccafaf
Chirayu Shah <chirayushah61@gmail.com>
2026-08-22 09:52:34 -0600

    removed default web browser mime

--- users/rayu/mime.nix
+++ users/rayu/mime.nix
@@ -33,10 +33,6 @@ in
     enable = true;
 
     defaultApplications = {
-      "text/html" = [ "zen.desktop" ];
-      "x-scheme-handler/http" = [ "zen.desktop" ];
-      "x-scheme-handler/https" = [ "zen.desktop" ];
-
       "application/pdf" = pdfViewer;
      
       "image/png" = imageViewer;
