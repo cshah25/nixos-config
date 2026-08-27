e8bf34b665adc612727c66fa0a20676cb95231b2
Chirayu Shah <chirayushah61@gmail.com>
2026-08-27 17:26:10 -0600

    enabled niri on thinkpad

--- hosts/NixThinkpad/default.nix
+++ hosts/NixThinkpad/default.nix
@@ -13,7 +13,7 @@
     desktop = {
       plasma.enable = false;
       gnome.enable = true;
-      niri.enable = false;
+      niri.enable = true;
       hyprland.enable = false;
     };
     gaming.enable = false;
