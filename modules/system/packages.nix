11aaf02e0ff1e666b1d50f25b57dbcfec6f4c8ba
Chirayu Shah <chirayushah61@gmail.com>
2026-09-14 22:20:19 -0600

    updated packages

--- modules/system/packages.nix
+++ modules/system/packages.nix
@@ -10,44 +10,14 @@
   config = {
     environment.systemPackages = with pkgs; [
       vim
+      man-pages
       curl
       wget
-      tree
-      alacritty
-      bat
-      tmux
-      fastfetch
-      swayidle
       sbctl
       brightnessctl
-      playerctl
-      adwaita-icon-theme
       xwayland-satellite
-      python3
-      ripgrep
-      wayland-pipewire-idle-inhibit
-      wakeonlan
-      seahorse
-      papirus-icon-theme
-      rclone
-      wl-clipboard
-    ] ++ lib.optionals (config.sys.desktop.niri.enable || config.sys.desktop.mango.enable) [
-      noctalia
-    ] ++ lib.optionals config.sys.desktop.hyprland.enable [
-      kitty
-    ] ++ lib.optionals config.sys.desktop.plasma.enable [
-      kdePackages.kwalletmanager
-    ] ++ lib.optionals config.sys.desktop.gnome.enable [
-      gnomeExtensions.blur-my-shell
-      gnomeExtensions.caffeine
-      gnomeExtensions.clipboard-indicator
-      gnomeExtensions.dash-to-dock
-      gnomeExtensions.appindicator
-      gnomeExtensions.multi-monitor-bar
-      gnomeExtensions.removable-drive-menu
     ] ++ lib.optionals (config.sys.desktop.gnome.enable && config.networking.hostName == "NixHome") [
       ddcutil
-      gnomeExtensions.brightness-control-using-ddcutil
     ];
   };
 }
--- users/rayu/packages.nix
+++ users/rayu/packages.nix
@@ -1,23 +1,52 @@
 { config, pkgs, osConfig, ... }:
 
 let
-  # Base packages (always installed)
+  # Base packages
   basePackages = with pkgs; [
-    xev
+    tree
+    bat
+    tmux
+    fastfetch
+    swayidle
+    playerctl
+    python3
+    ripgrep
+    wayland-pipewire-idle-inhibit
+    wakeonlan
+    wl-clipboard
+    rclone
+    seahorse
   ];
 in
 {
   home.packages = basePackages
     ++ (if osConfig.sys.desktop.plasma.enable then [
          pkgs.kdePackages.kamoso
+         pkgs.kdePackages.kwalletmanager
       ] else [])
     ++ (if osConfig.sys.services.remote.enable then [
          pkgs.moonlight-qt
       ] else [])
+    ++ (if osConfig.sys.desktop.hyprland.enable then [
+         pkgs.kitty
+      ] else [])
+    ++ (if osConfig.sys.desktop.mango.enable || osConfig.sys.desktop.niri.enable then [
+         pkgs.noctalia
+      ] else [])
     ++ (if osConfig.sys.desktop.gnome.enable then [
          pkgs.gnome-weather
          pkgs.gnome-tweaks
-      ] else []);
+         pkgs.gnomeExtensions.blur-my-shell
+         pkgs.gnomeExtensions.caffeine
+         pkgs.gnomeExtensions.clipboard-indicator
+         pkgs.gnomeExtensions.dash-to-dock
+         pkgs.gnomeExtensions.appindicator
+         pkgs.gnomeExtensions.multi-monitor-bar
+         pkgs.gnomeExtensions.removable-drive-menu
+      ] ++ (if osConfig.networking.hostName == "NixHome" then [
+         pkgs.gnomeExtensions.brightness-control-using-ddcutil
+      ] else [])
+    else []);
 
   programs.home-manager.enable = true;
 }
