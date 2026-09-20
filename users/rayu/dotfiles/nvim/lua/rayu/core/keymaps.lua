cf603b4952b6296c132e010b632d465bc9c3d97d
Chirayu Shah <chirayushah61@gmail.com>
2026-09-19 18:34:55 -0600

    added keybinds to move lines and block up or down

--- users/rayu/dotfiles/nvim/lua/rayu/core/keymaps.lua
+++ users/rayu/dotfiles/nvim/lua/rayu/core/keymaps.lua
@@ -22,3 +22,10 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tan
+
+vim.keymap.set('n', '<A-j>', ':m .+1<CR>==', { desc = 'Move line down' }) -- move line down
+vim.keymap.set('n', '<A-k>', ':m .-2<CR>==', { desc = 'Move line up' }) -- move line up
+
+vim.keymap.set('v', '<A-j>', ":m '>+1<CR>gv=gv", { desc = 'Move block down' }) -- move block down
+vim.keymap.set('v', '<A-k>', ":m '<-2<CR>gv=gv", { desc = 'Move block up' }) -- move block up
+
